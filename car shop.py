60a9536bdbc628f68ab90f150cbed0017ac2020b
ytoo883 <71450434+ytoo883@users.noreply.github.com>
2021-01-20 18:40:13 +0100

    Update car shop.py

--- car shop.py
+++ car shop.py
@@ -1,5 +1,28 @@
 Tesla_model_3 = '200,000 Dollars'
-Tesla_model_x = '500,000 Dollars'
+mercedes = '500,000 Dollars'
 Bmw_m_1450 = '1,000,000,000 Dollars'
 command = input('>Username: ')
-cars_available = input('>Tesla model x,Bmw m1450, ')
+print ('input car brand youy prefer')
+while :
+
+    cars_available = input('>Tesla ,Bmw ,mercedes ')
+    if cars_available.lower == 'Tesla':
+         price_tag = int(input('>pls what amount are you budgeted '))
+       if price_tag > 500000:
+          print ('>Not eligible')
+        else:
+           print('>Continue pls')
+          break
+     elif cars_available.lower == 'bmw':
+          if price_tag > 500000:
+          print ('>Not eligible')
+        else:
+           print('>Continue pls')
+          break
+    elif cars_available.lower == 'mercedes':
+          if price_tag > 500000:
+          print ('>Not eligible')
+        else:
+           print('>Continue pls')
+          break
+         
