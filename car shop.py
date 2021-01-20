673e62121050f7e5b77138c6bb247981edd181fa
ytoo883 <71450434+ytoo883@users.noreply.github.com>
2021-01-20 18:50:57 +0100

    Update car shop.py

--- car shop.py
+++ car shop.py
@@ -1,37 +1,37 @@
-
 print('''
-
-
 Tesla_model_3   = '200,000 Dollars'
 mercedes = '500,000 Dollars'
 Bmw_m_1450 = '1,000,000,000 Dollars'
 ''')
 command = input('>Username: ')
 print ('input car brand youy prefer')
-while :
 
-    cars_available = input('>Tesla ,Bmw ,mercedes ')
-    if cars_available.lower == 'Tesla':
-         price_tag = int(input('>pls what amount are you budgeted '))
+
+cars_available = input('>Tesla ,Bmw ,mercedes ')
+ if cars_available.lower == 'Tesla':
+     price_tag = int(input('>pls what amount are you budgeted '))
        if price_tag > 500000:
           print ('>Not eligible')
         else:
            print('>Continue pls')
-          break
+          
      elif cars_available.lower == 'bmw':
           if price_tag > 500000:
           print ('>Not eligible')
         else:
            print('>Continue pls')
-          break
+         
     elif cars_available.lower == 'mercedes':
           if price_tag > 500000:
           print ('>Not eligible')
         else:
            print('>Continue pls')
-          break
+       
 else:
     ('>No connection')
-break 
+
+contact = input ('pls you phone number or email')
+print (command +',well contact you @,'+ contact)
+
 
          
