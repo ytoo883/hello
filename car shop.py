abd1f46b051de249492d5c74130b8f9892f7cda5
ytoo883 <71450434+ytoo883@users.noreply.github.com>
2021-01-21 12:28:03 +0100

    Update car shop.py

--- car shop.py
+++ car shop.py
@@ -2,6 +2,18 @@ print('cars available')
 print('tesla,bmw,mercedes ')
 car_choosen = input ('pls type your intreset in options: ')
 while car_choosen != mercedes:
-  if car_choosen =='tesla':
-    price_tag
-         
+  if car_choosen.lower =='tesla':
+    price_tag = (500000)
+    print('what is yourbugdet in figures: ')
+    amount = int(input())
+    if amount > price_tag:
+      print('sorry were are not intrested')
+      break
+    else:
+      print('pls secure registration and continue')
+      break
+  elif car_choosen.lower == 'bmw':
+    price = (800000)
+    print('what yur planned budget')
+    bmw = int(input(''))
+    
